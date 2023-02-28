c2880420507a980b6bb5abd622a7e521082eb9e9
anjana6 <anjanashakthi95@gmail.com>
2023-02-28 22:07:04 +0530

    learn the data passing between component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component,ViewChild } from '@angular/core';
+import { PostComponent } from './post/post.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'angIntro';
+  parentMessage = 'pass data from input decorator'
+  childMessageFormPost = ''
+  outputChildMessage = ''
+
+  @ViewChild(PostComponent) childComp!:PostComponent;
+
+  ngAfterViewInit(){
+    this.childMessageFormPost = this.childComp.childMessage
+  }
+
+  receivedMessage(evnet:string){
+    this.outputChildMessage = evnet
+  }
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter,Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class PostComponent {
 
+  childMessage: string = 'from child to parent message'
+  outputChildMessage: string = 'from output child message'
+  imageUrl = 'https://popupsmart.com/encyclopedia/images/u/r/l/-/i/url-image-7a74f0de.png'
+  bool: boolean = true
+
+  @Input() fromParent: string = '';
+
+  @Output() messageEvent = new EventEmitter<string>()
+
+  passMessage(){
+    this.messageEvent.emit(this.outputChildMessage)
+  }
 }
