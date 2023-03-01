b244e28a6bc636848a9e4c695d260535e7c1034d
anjana6 <anjanashakthi95@gmail.com>
2023-03-01 18:37:23 +0530

    did the databinding task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { PostListComponent } from './post-list/post-list.component';
+import { PostItemComponent } from './post-item/post-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
-    PostListComponent
+    PostListComponent,
+    PostItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/post-item/post-item.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-item',
+  templateUrl: './post-item.component.html',
+  styleUrls: ['./post-item.component.css']
+})
+export class PostItemComponent {
+  postTitle = '';
+  postDetails = '';
+  imageUrl = '';
+  postUrl = '';
+  isBackground= false;
+}
