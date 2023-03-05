0e6225e2d6887915ea7f353845d5d0ca5211f052
uzafW5wYXzeMaQpfxH5v <anjanashakthi95@gmail.com>
2023-03-05 22:22:40 +0530

    Reactive form

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, NgModule } from '@angular/core';
-import { FormControl, NgForm, NgModel } from '@angular/forms';
+import { FormControl, NgForm, NgModel,FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -7,6 +7,27 @@ import { FormControl, NgForm, NgModel } from '@angular/forms';
   styleUrls: ['./forms.component.css']
 })
 export class FormsComponent {
+
+  form:any;
+  emailRegex:any ='xxxxxxxxxxxxxxxx';
+
+  constructor(){
+    this.form = new FormGroup({
+      email: new FormControl('',[
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(10),
+        Validators.email
+        // Validators.pattern(this.emailRegex)
+      ]),
+      password: new FormControl(),
+      address: new FormControl()
+    })
+  }
+
+  get Email(){
+    return this.form.email
+  }
 
   onSubmit(f:NgForm){
     console.log(f)
